f2aa7041bb2d31325f1473b80daa286f5a8b52e7
Ashutosh singh <ashutoshsinghsingh1256@gmail.com>
2026-02-27 20:06:47 +0530

    Add project files.

---
+++ DemoProject/Data/DbContext.cs
@@ -0,0 +1,14 @@
+﻿using DemoProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
